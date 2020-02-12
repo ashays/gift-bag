95921b53db24e226e973c1bceee2a14aaebb1337
ashays <ashay.sheth@gmail.com>
2020-02-11 23:58:31 -0500

    Updates

---
+++ precache-manifest.f2636d9f1f8f8a05fd2dd3c334784d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f54c52dc94561fe51172ca49b00bd8",
+    "url": "/gift-bag/index.html"
+  },
+  {
+    "revision": "3a8031c231c5edef3962",
+    "url": "/gift-bag/static/css/main.8631b1af.chunk.css"
+  },
+  {
+    "revision": "b68d5bd4159b35ba9629",
+    "url": "/gift-bag/static/js/2.ca21304c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gift-bag/static/js/2.ca21304c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8031c231c5edef3962",
+    "url": "/gift-bag/static/js/main.b1795c68.chunk.js"
+  },
+  {
+    "revision": "c38fa0aa3f509d7bb560",
+    "url": "/gift-bag/static/js/runtime-main.f946e2e4.js"
+  }
+]);
