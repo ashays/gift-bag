beda8e114b2c652f1f4fe746b8280ef3062239ee
Ashay Sheth <ashay.sheth@gmail.com>
2021-01-18 00:46:35 -0500

    catch-up old edits

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -15,11 +15,13 @@ class Gift extends React.Component {
   render() {
     return (
       <a className="gift" onClick={this.goToGift.bind(this)} href={this.props.link} target="_blank" alt={this.props.title} rel="noopener noreferrer">
+        <div className="save" onClick={this.saveGift.bind(this)}>
+          <svg width="17" height="21" viewBox="0 0 17 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.99658 1V0L0.996582 1H1.99658ZM15.9966 1H16.9966L15.9966 0V1ZM1.99658 20H0.996582L2.70369 20.7071L1.99658 20ZM8.99658 13L9.70369 12.2929H8.28948L8.99658 13ZM15.9966 20L15.2895 20.7071L16.9966 20H15.9966ZM1.99658 2H15.9966V0H1.99658V2ZM2.99658 13V1H0.996582V13H2.99658ZM2.99658 20V13H0.996582V20H2.99658ZM8.28948 12.2929L1.28948 19.2929L2.70369 20.7071L9.70369 13.7071L8.28948 12.2929ZM16.7037 19.2929L9.70369 12.2929L8.28948 13.7071L15.2895 20.7071L16.7037 19.2929ZM14.9966 13V20H16.9966V13H14.9966ZM14.9966 1V13H16.9966V1H14.9966Z" fill="white" fillOpacity="0.5"/>
+          </svg>
+        </div>
         <div className="title">{this.props.title}</div>
         <span>{this.props.description}</span>
-        <div className="save" onClick={this.saveGift.bind(this)}>
-          <svg viewBox="0 0 24 24"><path d="M17.016 18v-12.984h-10.031v12.984l5.016-2.203zM17.016 3q0.797 0 1.383 0.609t0.586 1.406v15.984l-6.984-3-6.984 3v-15.984q0-0.797 0.586-1.406t1.383-0.609h10.031z"></path></svg>
-        </div>
       </a>
     );
   }
--- src/components/Results.js
+++ src/components/Results.js
@@ -5,11 +5,13 @@ class Results extends React.Component {
   render() {
     return (
       <main>
-        <h2>Top Picks</h2>
+        <h2>Trending</h2>
         <Gift id="0A" title="Front Page Framed" link="https://store.nytimes.com/products/new-york-times-front-page-reprint" description="Straight from The Times archives, framed and unframed reprints are available of more than 58,000 historical front pages published since 1851." />
         <Gift id="4D" title="Bartender Kit" link="https://www.amazon.com/dp/B07B9KLRDC" description="A shaker, a jigger, and a mixer—everything one needs to master the art of mixing cocktails" />
         <Gift id="6E" title="Whiskey Glass Set" description="A quality set of whistkey glasses is what differentiates a hobbiest from a whiskey connoisseur" />
         <Gift id="2C" title="Travel-size DIY Cocktails" description="Treat them to their favorite drinks while 30,000 feet in the air" />
+        <h2>Top Picks</h2>
+        <h2>Featured</h2>
       </main>
     );
   }
