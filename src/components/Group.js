d12daf4f5298fbe61cc89df12c4b160fb63c5c26
Ashay Sheth <ashay.sheth@gmail.com>
2021-01-19 01:31:22 -0500

    only show persona link if missing gifts

--- src/components/Group.js
+++ src/components/Group.js
@@ -3,20 +3,24 @@ import './Group.css';
 import Gift from "./Gift";
 
 class Group extends React.Component {
-  render() {
-    return (
-        <div className="group">
-            <h2>For the <strong>{this.props.title}</strong></h2>
-            <p>{this.props.description}</p>
-            {this.props.gifts.map(function(gift, i) {
-                return (
-                    <Gift title={gift.name} link={gift.link} brand={gift.brand} price={gift.price} key={i} />
-                );
-            })}
-            <a href="#" className="ghost-btn">See all {this.props.noGifts} gift ideas</a>
-        </div>
-    );
-  }
+    render() {
+        let button;
+        if (this.props.noGifts > 2) {
+            button = (<a href="#" className="ghost-btn">See all {this.props.noGifts} gift ideas</a>);
+        }
+        return (
+            <div className="group">
+                <h2>For the <strong>{this.props.title}</strong></h2>
+                <p>{this.props.description}</p>
+                {this.props.gifts.map(function(gift, i) {
+                    return (
+                        <Gift title={gift.name} link={gift.link} brand={gift.brand} price={gift.price} key={i} />
+                    );
+                })}
+                {button}
+            </div>
+        );
+    }
 }
 
 export default Group;
