ab1a5e86f20968b77aa2782145ea4a6e35e9abf7
Ashay Sheth <ashay.sheth@gmail.com>
2021-01-27 02:01:05 -0500

    added entry animation for sheet and abstracted component (moved inner elements out)

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ class App extends React.Component {
           <div className="subtitle">Gift Guide</div>
         </header>
         <Main />
-        <Sheet />
+        <Sheet close={<div className="button">Start browsing gifts</div>}>
+          <h2>Looking for a gift?</h2>
+          <p>We believe in a better way to find the perfect gift—one that focuses on the humans instead of the products. Explore a curated selection of hand-selected items, each meeting our rigorous standards of a thoughtful gift. We may earn an affiliate commission if you buy something using these links.</p>
+        </Sheet>
       </div>
     );
   }
--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -4,10 +4,12 @@ import './Sheet.css';
 class Sheet extends React.Component { 
   constructor(props) {
     super(props);
-    this.state = {
-      isOpen: true
-    }
+    this.state = { isOpen: false };
     this.closeSheet = this.closeSheet.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout( () => { this.setState({isOpen: true}); }, 300);
   }
 
   closeSheet() {
@@ -19,9 +21,8 @@ class Sheet extends React.Component {
       <div className={this.state.isOpen ? "slide open" : "slide closed"}>
         <div className="overlay" onClick={this.closeSheet} />
         <div className="drawer">
-          <h2>Looking for a gift?</h2>
-          <p>We believe in a better way to find the perfect gift—one that focuses on the humans instead of the products. Explore a curated selection of hand-selected items, each meeting our rigorous standards of a thoughtful gift. We may earn an affiliate commission if you buy something using these links.</p>
-          <div className="button" onClick={this.closeSheet}>Start browsing gifts</div>
+          {this.props.children}
+          {this.props.close && (<div onClick={this.closeSheet}>{this.props.close}</div>)}
         </div>
       </div>
     );
